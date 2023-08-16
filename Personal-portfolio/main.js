10ca6da8324fd38035b06ccbfab23e96d6d0f1d9
kristinehetler <140378663+kristinehetler@users.noreply.github.com>
2023-08-17 00:02:07 +0800

    Add files via upload

---
+++ Personal-portfolio/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+})(jQuery);
